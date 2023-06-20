47c6445284abcc4617a88ca1bd4559c16b89c375
JP Flores <jpflores013@gmail.com>
2023-06-20 22:11:41 +0000

    init commit

---
+++ R/install-packages.R
@@ -0,0 +1,11 @@
+## packages needed for base project
+install.packages("devtools")
+devtools::install_github("https://github.com/jpflores-13/rclass.git",
+                         dependencies = T,
+                         build_opts = "")
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("tidytuesdayR")
+install.packages("quarto")
+install.packages("renv")
+renv::snapshot()
